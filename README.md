bf8be85007010338072b2a9941fcafff8a8cca68
tiagoarodrigues55 <git@stackblitz.com>
2024-10-10 17:22:55 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# moveo-case-teste
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/tiagoarodrigues55/moveo-case-teste)
---
+++ moveo-case/pages/api/get-joke.js
@@ -0,0 +1,23 @@
+// pages/api/get-joke.js
+
+const JokeAPI = require('sv443-joke-api');
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const response = await JokeAPI.getJokes()
+        .then((res) => res.json())
+        .then((data) => {
+          res.status(200).json(data);
+        });
+
+      // Retorna o conteúdo da resposta recebida
+    } catch (error) {
+      // Em caso de erro, retorna a mensagem de erro
+      res.status(500).json({ error: error.message });
+    }
+  } else {
+    // Retorna 405 se o método não for GET
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
