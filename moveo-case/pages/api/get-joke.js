72a16ad23f6807193e923c8242a9b2795f82b6a1
TIAGO AMERICANO RODRIGUES <58439632+tiagoarodrigues55@users.noreply.github.com>
2024-10-10 18:13:45 -0300

    Update get-joke.js

--- moveo-case/pages/api/get-joke.js
+++ moveo-case/pages/api/get-joke.js
@@ -3,7 +3,6 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
     try {
       const response = await axios.get(
         'https://v2.jokeapi.dev/joke/Any?lang=pt',
@@ -21,9 +20,4 @@ export default async function handler(req, res) {
       // Em caso de erro, retorna a mensagem de erro
       res.status(500).json({ error: error.message });
     }
-  } else {
-    // Retorna 405 se o método não for GET
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
 }
