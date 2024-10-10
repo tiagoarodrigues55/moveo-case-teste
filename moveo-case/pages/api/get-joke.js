22cac6c5228ba2614030708b4fdce413462eb727
TIAGO AMERICANO RODRIGUES <58439632+tiagoarodrigues55@users.noreply.github.com>
2024-10-10 18:02:13 -0300

    Update get-joke.js

--- moveo-case/pages/api/get-joke.js
+++ moveo-case/pages/api/get-joke.js
@@ -1,16 +1,22 @@
 // pages/api/get-joke.js
 
-const JokeAPI = require('sv443-joke-api');
+import axios from 'axios';
+
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
-      const response = await JokeAPI.getJokes({language: "pt"})
-        .then((res) => res.json())
-        .then((data) => {
-          res.status(200).json(data);
-        });
+      const response = await axios.post(
+        'https://v2.jokeapi.dev/joke/Any?lang=pt',
+        {},
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
 
       // Retorna o conteúdo da resposta recebida
+      res.status(200).json(response.data);
     } catch (error) {
       // Em caso de erro, retorna a mensagem de erro
       res.status(500).json({ error: error.message });
